de244ef978835b638f67927e31b8bcc8f9bf2cc4
stebby70 <153183086+stebby70@users.noreply.github.com>
2024-11-18 16:02:48 +0700

    Add files via upload

---
+++ Fishing - Refactor/src/Rod.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+
+public class Rod {
+	String name, brand;
+	int length, successRate;
+	
+	public Rod(String name, String brand, int length, int successRate) {
+		super();
+		this.name = name;
+		this.brand = brand;
+		this.length = length;
+		this.successRate = successRate;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public int getSuccessRate() {
+		return successRate;
+	}
+	public void setSuccessRate(int successRate) {
+		this.successRate = successRate;
+	}
+	public int randomizeSuccessRate() {
+		Random random = new Random();
+		return random.nextInt(50) + 1;
+	}
+	
+
+
+	
+	
+}
